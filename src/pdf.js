219d0a88a74533ee01d5b03d9ec2484362708e4a
Joerg Schneider <joerg.schneider@zeppelin.com>
2020-04-20 09:15:01 +0200

    fix missing dir, add some info output, add style manipulation

--- src/pdf.js
+++ src/pdf.js
@@ -1,13 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const puppeteer = require("puppeteer");
+const path = require('path');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-const dummySource =
-  "https://www.zeppelin-cat.de/gebrauchtmaschinen/details/caterpillar-320fl-nhd10073.html";
-const dummyFile = "example.pdf";
+const dummySource = 'https://www.example.com';
+const dummyFile = 'example';
 
-app.get("/download", (req, res) => {
+const dir = './tmp';
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+
+app.get('/', (req, res) => {
+  res.send('use /download?source={html_page_url}&output={filename} to generate pdf and download it');
+});
+
+app.get('/download', (req, res) => {
   generatePDF(req, res);
 });
 
@@ -17,16 +26,20 @@ app.listen(3000, () => {
 
 async function generatePDF(req, res) {
   let source = req.query.source || dummySource;
-  let output = `${req.query.output}.pdf` || dummyFile;
+  let output = `${req.query.output || dummyFile}.pdf`;
+
+  console.log(`Generating PDF "${output}" from "${source}"`);
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(source, {
-    waitUntil: "networkidle2"
+    waitUntil: 'networkidle2',
   });
-  await page.pdf({ path: path.join("./pdf", output), format: "A4" });
+  await page.addStyleTag({ path: path.resolve(__dirname, 'print.css') });
+  await page.pdf({ path: path.join(dir, output), format: 'A4' });
   await browser.close();
 
-  let fileLocation = await path.join("./pdf", output);
-  await console.log(fileLocation);
+  let fileLocation = await path.join(dir, output);
+  // await console.log(fileLocation);
   await res.download(fileLocation, output);
 }
